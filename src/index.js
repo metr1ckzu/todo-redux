3d95bcdea616f9046e9469c50c6fe3ba4d0f5082
metr1ckzu <metr1ckzu@gmail.com>
2017-09-19 19:30:12 +0600

    deleted outdated import

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import todoApp from './reducers'
 import App from './components/App'
 
-const store = createStore(todoApp, applyMiddleware(logger, thunk))
+const store = createStore(todoApp, applyMiddleware(thunk))
 
 render(
   <Provider store={store}>
